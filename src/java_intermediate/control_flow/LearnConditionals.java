8fedfc7c1a5a014bebad037a6967679c51db92ce
samipiit <sami@piit.us>
2022-03-14 22:25:55 -0400

    Added additional comment in LearnConditionals class

--- src/java_intermediate/control_flow/LearnConditionals.java
+++ src/java_intermediate/control_flow/LearnConditionals.java
@@ -10,10 +10,19 @@ public class LearnConditionals {
     }
 
     static int doSubtraction(int num1, int num2) {
+        /*
+         If num1 is NOT greater than or equal to num2, then we KNOW that num2 is greater than num1. This is why we
+         don't need an else condition.
+
+         REMINDER: Once a method hits any return statement, the method execution stops. Java will not execute a method
+                   any further than a return statement.
+         */
+
         if (num1 >= num2) {
             return num1 - num2;
         }
 
         return num2 - num1;
     }
+
 }
